f71ffdc59f0ad991677f423be764e4ab09bd7f4a
Łukasz Okoń <l.okon@haso.pl>
2021-08-02 18:44:30 +0200

    feature: add check_email function with re

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import re
+
+
+def check_email(ciag_znakow: str):
+    if len(ciag_znakow) > 255:
+        return False
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.match(pattern, ciag_znakow)
 
 if __name__ == '__main__':
-    pass
+    adres = 'jan.kowalski@sda.pl'
+    print(check_email(adres))
